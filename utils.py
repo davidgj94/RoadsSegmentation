51760aba3c2f91ccee76998521071cc85f7ccf65
David <davidgj1994@gmail.com>
2018-11-25 19:21:30 +0100

    arreglando _get_next_point porque np.where devuelve tupla en vez de lista y el zip no funciona

--- utils.py
+++ utils.py
@@ -9,10 +9,13 @@ import sys
 import matplotlib.pyplot as plt
 import pdb
 from skimage import morphology
-from sklearn.neighbors import KDTree
 import networkx as nx
 import cv2
 from matplotlib.patches import Circle
+import mahotas as mah
+from skimage import measure
+from scipy.spatial.distance import cdist
+
 
 base_URL = "https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDvgF0JSBrlYLDzY7pPqtcBSgGslmaAlzw&zoom=19&format=png&maptype=roadmap&style=color:0x000000&style=element:labels%7Cvisibility:off&style=feature:road%7Celement:geometry%7Ccolor:0xffffff%7Cvisibility:on&style=feature:road.highway%7Celement:geometry%7Ccolor:0xffffff%7Cvisibility:on&style=feature:road.local%7Celement:geometry%7Cvisibility:off&size=640x640&scale=2"
 
@@ -35,24 +38,126 @@ def download_img_mask(coord):
     
     return img, mask
 
-def skeleton_endpoints(skel):
-    # make out input nice, possibly necessary
-    skel = skel.copy()
-    skel[skel!=0] = 1
-    skel = np.uint8(skel)
+def _get_next_point(skel, current_point, prev_point=None):
+    
+    upper_limit = current_point[0]-1
+    bottom_limit = current_point[0]+2
+    left_limit = current_point[1]-1
+    right_limit = current_point[1]+2
+    neighbors = skel[upper_limit:bottom_limit, left_limit:right_limit]
+    
+    neighbors_coord = zip(np.where(neighbors))
+    pdb.set_trace()
+    neighbors_coord = [point_coord for point_coord in neighbors_coord if point_coord not in [(1,1), prev_point]]
+    pdb.set_trace()
+    next_point = neighbors_coord[min(cdist([(1,1)], neighbors_coord))]
+    return next_point[0] - 1, next_point[0] - 1
+    
 
-    # apply the convolution
-    kernel = np.uint8([[1,  1, 1],
-                       [1, 10, 1],
-                       [1,  1, 1]])
-    src_depth = -1
-    filtered = cv2.filter2D(skel,src_depth,kernel)
+def sort_skeleton(skel):
+    
+    skel_labeled, num_labels = measure.label(skel, connectivity=2, return_num=True)
+    sorted_coord_labels = []
+    
+    for i in range(num_labels):
+        
+        skel_cc = (skel_labeled == (i+1))
+        
+        _, ep = detect_br_ep(skel_cc)
+        ep_y, ep_x = np.where(ep)
+        ep_coords = zip(ep_y, ep_x)
+        sorted_coord = []
+        sorted_coord.append(ep_coords[0])
+        next_point = _get_next_point(skel_cc, sorted_coord[-1])
+        sorted_coord.append(next_point)
+        while sorted_coord[-1] == ep_coords[1]:
+            next_point = _get_next_point(skel, sorted_coord[-1], sorted_coord[-2])
+            
+            sorted_coord.append(next_point)
+            
+        sorted_coord_labels.append(sorted_coord)
+        
+    return sorted_coord_labels
 
-    # now look through to find the value of 11
-    # this returns a mask of the endpoints, but if you just want the coordinates, you could simply return np.where(filtered==11)
-    #out = np.zeros_like(skel)
-    #out[np.where(filtered==11)] = 1
-    return np.where(filtered==11)
+def test_sort_skeleton():
+    
+    skel = np.array([[0,0,0,0,0],
+                     [0,0,0,1,0],
+                     [0,1,0,0,1],
+                     [0,0,1,1,0],
+                     [0,0,0,0,0]])
+    
+    sorted_points = sort_skeleton(skel)
+    pdb.set_trace()
+    print "Fin"
+    
+def detect_br_ep(sk):
+    
+    branch1=np.array([[2, 1, 2], [1, 1, 1], [2, 2, 2]])
+    branch2=np.array([[1, 2, 1], [2, 1, 2], [1, 2, 1]])
+    branch3=np.array([[1, 2, 1], [2, 1, 2], [1, 2, 2]])
+    branch4=np.array([[2, 1, 2], [1, 1, 2], [2, 1, 2]])
+    branch5=np.array([[1, 2, 2], [2, 1, 2], [1, 2, 1]])
+    branch6=np.array([[2, 2, 2], [1, 1, 1], [2, 1, 2]])
+    branch7=np.array([[2, 2, 1], [2, 1, 2], [1, 2, 1]])
+    branch8=np.array([[2, 1, 2], [2, 1, 1], [2, 1, 2]])
+    branch9=np.array([[1, 2, 1], [2, 1, 2], [2, 2, 1]])
+    
+    endpoint1=np.array([[0, 0, 0], [0, 1, 0], [2, 1, 2]])
+    endpoint2=np.array([[0, 0, 0], [0, 1, 2], [0, 2, 1]])
+    endpoint3=np.array([[0, 0, 2], [0, 1, 2], [0, 2, 1]])
+    endpoint4=np.array([[0, 2, 1], [0, 1, 2], [0, 0, 0]])
+    endpoint5=np.array([[2, 1, 2], [0, 1, 0], [0, 0, 0]])
+    endpoint6=np.array([[1, 2, 0], [2, 1, 0], [0, 0, 0]])
+    endpoint7=np.array([[2, 0, 0], [1, 1, 0], [2, 0, 0]])
+    endpoint8=np.array([[0, 0, 0], [2, 1, 0], [1, 2, 0]])
+    endpoint9=np.array([[0, 0, 0], [0, 1, 1], [0, 0, 2]])
+    
+    br1=mah.morph.hitmiss(sk,branch1)
+    br2=mah.morph.hitmiss(sk,branch2)
+    br3=mah.morph.hitmiss(sk,branch3)
+    br4=mah.morph.hitmiss(sk,branch4)
+    br5=mah.morph.hitmiss(sk,branch5)
+    br6=mah.morph.hitmiss(sk,branch6)
+    br7=mah.morph.hitmiss(sk,branch7)
+    br8=mah.morph.hitmiss(sk,branch8)
+    br9=mah.morph.hitmiss(sk,branch9)
+    
+    ep1=mah.morph.hitmiss(sk,endpoint1)
+    ep2=mah.morph.hitmiss(sk,endpoint2)
+    ep3=mah.morph.hitmiss(sk,endpoint3)
+    ep4=mah.morph.hitmiss(sk,endpoint4)
+    ep5=mah.morph.hitmiss(sk,endpoint5)
+    ep6=mah.morph.hitmiss(sk,endpoint6)
+    ep7=mah.morph.hitmiss(sk,endpoint7)
+    ep8=mah.morph.hitmiss(sk,endpoint8)
+    ep9=mah.morph.hitmiss(sk,endpoint9)
+    
+    br = br1 + br2 + br3 + br4 + br5 + br6 + br7 + br8 + br9
+    ep = ep1 + ep2 + ep3 + ep4 + ep5 + ep6 + ep7 + ep8 + ep9
+    
+    return br, ep
+    
+test_sort_skeleton()
+
+#def skeleton_endpoints(skel):
+    ## make out input nice, possibly necessary
+    #skel = skel.copy()
+    #skel[skel!=0] = 1
+    #skel = np.uint8(skel)
+
+    ## apply the convolution
+    #kernel = np.uint8([[1,  1, 1],
+                       #[1, 10, 1],
+                       #[1,  1, 1]])
+    #src_depth = -1
+    #filtered = cv2.filter2D(skel,src_depth,kernel)
+
+    ## now look through to find the value of 11
+    ## this returns a mask of the endpoints, but if you just want the coordinates, you could simply return np.where(filtered==11)
+    ##out = np.zeros_like(skel)
+    ##out[np.where(filtered==11)] = 1
+    #return np.where(filtered==11)
 
 #def get_skeleton_graph(X):
     
@@ -79,48 +184,64 @@ def skeleton_endpoints(skel):
 
     
 
-img, mask = download_img_mask(coord)
-mask = mask[:1200,:]
+#img, mask = download_img_mask(coord)
+#mask = mask[:1200,:]
 
-plt.figure()
-plt.imshow(img)
+#plt.figure()
+#plt.imshow(img)
 
-plt.figure()
-plt.imshow(mask)
-
-
-#pdb.set_trace()
-skeleton = morphology.skeletonize(mask == 255)
-
-plt.figure()
-plt.imshow(skeleton)
+#plt.figure()
+#plt.imshow(mask)
 
 
-skeleton = morphology.medial_axis(mask == 255)
+##pdb.set_trace()
+#skeleton = morphology.skeletonize(mask == 255)
 
-plt.figure()
-plt.imshow(skeleton)
+#plt.figure()
+#plt.imshow(skeleton)
 
 
-skeleton[0,:] = 0
-skeleton[-1,:] = 0
-skeleton[:,0] = 0
-skeleton[:,-1] = 0
+#skeleton = morphology.medial_axis(mask == 255)
 
-fig,ax = plt.subplots(1)
-ax.imshow(skeleton)
+#plt.figure()
+#plt.imshow(skeleton)
 
-y, x = skeleton_endpoints(skeleton)
 
-for xx,yy in zip(x,y):
-    circ = Circle((xx,yy),7)
-    ax.add_patch(circ)
+#skeleton[0,:] = 0
+#skeleton[-1,:] = 0
+#skeleton[:,0] = 0
+#skeleton[:,-1] = 0
 
-#skeleton_list = np.where(skeleton > 0)
-#pdb.set_trace()
-#get_skeleton_graph(skeleton_list)
+#fig,ax = plt.subplots(1)
+#ax.imshow(skeleton)
 
-plt.show()
+##y, x = skeleton_endpoints(skeleton)
+
+#br, ep = detect_br_ep(skeleton)
+
+#y, x = np.where(ep)
+
+#for xx,yy in zip(x,y):
+    #circ = Circle((xx,yy),20)
+    #ax.add_patch(circ)
+
+##skeleton_list = np.where(skeleton > 0)
+##pdb.set_trace()
+##get_skeleton_graph(skeleton_list)
+
+## Display the image and plot all contours found
+#fig, ax = plt.subplots()
+#ax.imshow(skeleton, interpolation='nearest', cmap=plt.cm.gray)
+#contours = measure.find_contours(skeleton, 0)
+#for n, contour in enumerate(contours):
+    #ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
+
+#ax.axis('image')
+#ax.set_xticks([])
+#ax.set_yticks([])
+#plt.show()
+
+#plt.show()
         
 
             
